c4b9223b2443cb3a6550e7ab9afef9a4127824cd
joaohsilva <joaohasilva416@gmail.com>
2026-05-19 20:52:27 -0300

    feat: add main function

--- src/jobicy_api.py
+++ src/jobicy_api.py
@@ -15,7 +15,7 @@ class JobicyAPI:
 
 
     def fetch_data(self) -> None:
-        url = f"{self.base_url}?count={self.count}&{self.industry}"
+        url = f"{self.base_url}?count={self.count}&industry={self.industry}"
         response = requests.get(url)
 
         if response.status_code == 200:
@@ -31,3 +31,21 @@ class JobicyAPI:
         
         else:
             return pd.DataFrame()
+
+
+def main() -> None:
+    api = JobicyAPI(
+        base_url= "https://jobicy.com/api/v2/remote-jobs",
+        industry= "dev",
+        count= 10
+    )
+
+    api.fetch_data()
+
+    jobs_df = api.get_jobs_data()
+
+    print(jobs_df)
+
+
+if __name__ == '__main__':
+    main()
