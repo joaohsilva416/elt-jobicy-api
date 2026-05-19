7dfde7796d035311a39d3c2486fb6134d175457a
joaohsilva <joaohasilva416@gmail.com>
2026-05-19 19:38:31 -0300

    feat: jobicy api class and constructor

--- src/jobicy_api.py
+++ src/jobicy_api.py
@@ -4,3 +4,11 @@ from sqlalchemy import create_engine
 from snowflake.sqlalchemy import URL
 from dotenv import load_dotenv
 import os
+
+
+class JobicyAPI:
+    def __init__(self, base_url: str, industry: str, count: int) -> None:
+        self.base_url = base_url
+        self.industry = industry
+        self.count = count
+        self.data = None
