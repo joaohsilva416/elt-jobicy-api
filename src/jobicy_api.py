4dc4fc8ab3401533d1a8cf026d3a4d0b20f90282
joaohsilva <joaohasilva416@gmail.com>
2026-05-20 00:01:19 -0300

    feat: add data transformation and type casting before load

--- src/jobicy_api.py
+++ src/jobicy_api.py
@@ -44,26 +44,26 @@ class JobicyAPI:
 
 
 class Snowflake:
-    def __init__(self, account: str, user: str, password: str, database: str, schema: str, warehouse: str):
+    def __init__(self, account: str, user: str, password: str, database: str, schema: str, warehouse: str) -> None:
         self.engine = create_engine(URL(
-            account = account,
-            user = user,
-            password = password,
-            database = database,
-            schema = schema,
-            warehouse = warehouse,
+            account=account,
+            user=user,
+            password=password,
+            database=database,
+            schema=schema,
+            warehouse=warehouse
         ))
 
     
-    def save_to_snowflake(self, df: pd.DataFrame, table_name: str) :
-        df.to_sql(table_name, self.engine, if_exists= 'replace', index=False)
+    def save_to_snowflake(self, df: pd.DataFrame, table_name: str) -> None:
+        df.to_sql(table_name, self.engine, if_exists='replace', index=False)
         
 
 def main() -> None:
     api = JobicyAPI(
-        base_url= "https://jobicy.com/api/v2/remote-jobs",
-        industry= "dev",
-        count= 10
+        base_url="https://jobicy.com/api/v2/remote-jobs",
+        industry="data-science",
+        count=10
     )
 
     api.fetch_data()
@@ -72,13 +72,61 @@ def main() -> None:
 
     if not jobs_df.empty:
         
+        # Rename columns to follow camelCase naming convention
+        jobs_df.columns = [
+            'ID',
+            'URL',
+            'companyLogo',
+            'companyName',
+            'jobDescription',
+            'jobExcerpt',
+            'jobGeo',
+            'jobIndustry',
+            'jobLevel',
+            'jobSlug',
+            'jobTitle',
+            'jobType',
+            'pubDate',
+            'salaryCurrency',
+            'annualSalaryMin',
+            'annualSalaryMax',
+            'salaryPeriod'
+        ]
+
+        jobs_df['ID'] = jobs_df['ID'].astype(int)
+
+        # Cast all columns to string to ensure Snowflake compatibility
+        string_columns = [
+            'URL',
+            'companyLogo',
+            'companyName',
+            'jobDescription',
+            'jobExcerpt',
+            'jobGeo',
+            'jobIndustry',
+            'jobLevel',
+            'jobSlug',
+            'jobTitle',
+            'jobType',
+            'pubDate',
+            'salaryCurrency',
+            'annualSalaryMin',
+            'annualSalaryMax',
+            'salaryPeriod'
+            ]
+        
+        for column in string_columns:
+            jobs_df[column] = jobs_df[column].astype(str)
+        
+        jobs_df = jobs_df.fillna('')
+        
         saver = Snowflake(
-            account= get_env('ACCOUNT'),
-            user= get_env('USER'),
-            password= get_env('PASSWORD'),
-            database= get_env('DATABASE'),
-            schema= get_env('SCHEMA'),
-            warehouse= get_env('WAREHOUSE'),
+            account=get_env('ACCOUNT'),
+            user=get_env('USER'),
+            password=get_env('PASSWORD'),
+            database=get_env('DATABASE'),
+            schema=get_env('SCHEMA'),
+            warehouse=get_env('WAREHOUSE')
         )
 
         saver.save_to_snowflake(jobs_df, table_name='remote_works')
