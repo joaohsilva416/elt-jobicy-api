0492418148f1ec81cbba7c19e375d26f3fcdb3f9
joaohsilva <joaohasilva416@gmail.com>
2026-05-19 23:14:29 -0300

    feat: add Snowflake connection class and env helper function

--- src/jobicy_api.py
+++ src/jobicy_api.py
@@ -6,6 +6,16 @@ from dotenv import load_dotenv
 import os
 
 
+load_dotenv()
+
+
+def get_env(key: str) -> str:
+    value = os.getenv(key)
+    if value is None:
+        raise ValueError(f"Missing required environment variable: {key}")
+    return value
+
+
 class JobicyAPI:
     def __init__(self, base_url: str, industry: str, count: int) -> None:
         self.base_url = base_url
@@ -33,6 +43,22 @@ class JobicyAPI:
             return pd.DataFrame()
 
 
+class Snowflake:
+    def __init__(self, account: str, user: str, password: str, database: str, schema: str, warehouse: str):
+        self.engine = create_engine(URL(
+            account = account,
+            user = user,
+            password = password,
+            database = database,
+            schema = schema,
+            warehouse = warehouse,
+        ))
+
+    
+    def save_to_snowflake(self, df: pd.DataFrame, table_name: str) :
+        df.to_sql(table_name, self.engine, if_exists= 'replace', index=False)
+        
+
 def main() -> None:
     api = JobicyAPI(
         base_url= "https://jobicy.com/api/v2/remote-jobs",
@@ -44,8 +70,22 @@ def main() -> None:
 
     jobs_df = api.get_jobs_data()
 
-    print(jobs_df)
+    if not jobs_df.empty:
+        
+        saver = Snowflake(
+            account= get_env('ACCOUNT'),
+            user= get_env('USER'),
+            password= get_env('PASSWORD'),
+            database= get_env('DATABASE'),
+            schema= get_env('SCHEMA'),
+            warehouse= get_env('WAREHOUSE'),
+        )
 
+        saver.save_to_snowflake(jobs_df, table_name='remote_works')
+        print('Data successfully saved on Snowflake')
+    
+    else:
+        print('There is no data to be saved on Snowflake')
 
 if __name__ == '__main__':
     main()
